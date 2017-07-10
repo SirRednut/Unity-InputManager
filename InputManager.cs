8a6206a2efa2966d521b34990665056987cfc50b
Alex Bryant <1005677@student.sae.edu.au>
2017-07-11 09:04:17 +1000

    Added support for GetKeyDown and GetKeyUp for tapping and releasing instead of just holding. Whoops!

--- InputManager.cs
+++ InputManager.cs
@@ -12,9 +12,7 @@ public class InputManager : MonoBehaviour {
 						ipt_Escape, ipt_jump, ipt_sprint, ipt_interact,
 						ipt_inventory, ipt_click;*/
 
-	[SerializeField]
-	
-	private List<InputData> inputList;
+	public List<InputData> inputList; // Comment out this line if not wanting an extra list in inspector
 
 	void Start () {
 		//Uncomment this if not wanting an extra list in inspector
@@ -29,11 +27,35 @@ public class InputManager : MonoBehaviour {
 	{
 		//Check every input and every function run
 		foreach(InputData ipt in inputList)
-			if (Input.GetKey(ipt.Key1) || Input.GetKey(ipt.Key2))
-				foreach (UnityEvent iptEvent in ipt.events)
-				{
-					iptEvent.Invoke();
-				}
+		{
+			if (ipt.HoldEvents.Length > 0)
+			{
+				if (Input.GetKey(ipt.Key1) || Input.GetKey(ipt.Key2))
+					foreach (UnityEvent iptEvent in ipt.HoldEvents)
+					{
+						iptEvent.Invoke();
+					}
+			}
+
+			if (ipt.TapEvents.Length > 0)
+			{
+				if (Input.GetKeyDown(ipt.Key1) || Input.GetKeyDown(ipt.Key2))
+					foreach (UnityEvent iptEvent in ipt.TapEvents)
+					{
+						iptEvent.Invoke();
+					}
+			}
+
+
+			if (ipt.ReleaseEvents.Length > 0)
+			{
+				if (Input.GetKeyUp(ipt.Key1) || Input.GetKeyUp(ipt.Key2))
+					foreach (UnityEvent iptEvent in ipt.ReleaseEvents)
+					{
+						iptEvent.Invoke();
+					}
+			}
+		}
 
 	}
 }
@@ -52,6 +74,12 @@ public class InputData{
 	[TooltipAttribute("Select what keycodes to use for this input")]
 	public KeyCode Key2;
 
-	[TooltipAttribute("Function Calls Are Done Here")]
-	public UnityEvent[] events;
+	[TooltipAttribute("Function Calls Are Done Here When Button Is Held")]
+	public UnityEvent[] HoldEvents;
+
+	[TooltipAttribute("Function Calls Are Done Here When Button Is Pressed Once")]
+	public UnityEvent[] TapEvents;
+
+	[TooltipAttribute("Function Calls Are Done Here When Button Is Released")]
+	public UnityEvent[] ReleaseEvents;
 }
